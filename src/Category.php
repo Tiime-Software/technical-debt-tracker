7cadd655d9756afd74de7559e9ac8c4d7d32aedb
Flavien Rodrigues <rodrigues.flavien@gmail.com>
2021-12-22 18:04:33 +0100

    lint

--- src/Category.php
+++ src/Category.php
@@ -56,7 +56,8 @@ final class Category
         return new self(self::NEED_TESTS, 150);
     }
 
-    public static function delayedRefactoring(): self{
+    public static function delayedRefactoring(): self
+    {
         return new self(self::DELAYED_REFACTORING, 100);
     }
 
